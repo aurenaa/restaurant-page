82d7974e9713a10295c1f8b44a8e3116aefd744a
aurenaa <salapuranev@gmail.com>
2025-06-12 11:54:31 +0200

    menu header

--- src/menu.js
+++ src/menu.js
@@ -3,9 +3,28 @@ function createMenuPage() {
     const pageContent = document.createElement("div");
     pageContent.classList.add("page-content");
 
+    const menuHead = document.createElement("div");
+    menuHead.classList.add("menu-head");
+    content.appendChild(menuHead);
+
+    //const ourMenu = document.createElement("div");
+    //ourMenu.classList.add(our-menu);
+    //menuHead.appendChild(ourMenu);
+
+    const menuImage = document.createElement("img");
+    menuImage.classList.add("menu-img");
+    menuImage.src = "/menu-img.jpeg";
+    menuHead.appendChild(menuImage);
+
+    const menuUnderline = document.createElement("img");
+    menuUnderline.classList.add("under-line");
+    menuUnderline.src = "/underline.png";
+    menuHead.appendChild(menuUnderline);
+
     const headline = document.createElement("h1");
     headline.textContent = "Our Menu";
-    pageContent.appendChild(headline);
+    headline.classList.add("head-line");
+    menuHead.appendChild(headline);
 
     const menuList = document.createElement("ul");
     const menuItem1 = document.createElement("li");
