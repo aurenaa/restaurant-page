530f6f5391323fdf319353c7d8d007453725bc39
aurenaa <salapuranev@gmail.com>
2025-06-13 17:31:28 +0200

    pizza menu

--- src/menu.js
+++ src/menu.js
@@ -40,19 +40,19 @@ function createMenuPage() {
 
     // Pepperoni Pizza
     const pepperoni = document.createElement("div");
-    pepperoni.classList.add("pepperoni");
+    pepperoni.classList.add("pizza");
     pizzaDetails.appendChild(pepperoni);
 
     const pepperoniInfo = document.createElement("div");
-    pepperoniInfo.classList.add("pepperoni-info");
+    pepperoniInfo.classList.add("pizza-info");
     pepperoni.appendChild(pepperoniInfo);
 
     const pepperoniName = document.createElement("h3");
-    pepperoniName.textContent = "Pepperoni -";
+    pepperoniName.textContent = "PEPPERONI -";
     pepperoniInfo.appendChild(pepperoniName);
 
     const pepperoniPrices = document.createElement("p");
-    pepperoniPrices.classList.add("pepperoni-price");
+    pepperoniPrices.classList.add("pizza-price");
     pepperoniPrices.textContent = "Per slice: $3.50, Whole pie: $20.00";
     pepperoniInfo.appendChild(pepperoniPrices);
 
@@ -60,7 +60,140 @@ function createMenuPage() {
     pepperoniIngredients.textContent = "(Pizza sauce, mozzarella, pepperoni)";
     pepperoni.appendChild(pepperoniIngredients);
 
+    // Supreme pizza
+    const supreme = document.createElement("div");
+    supreme.classList.add("pizza");
+    pizzaDetails.appendChild(supreme);
+
+    const supremeInfo = document.createElement("div");
+    supremeInfo.classList.add("pizza-info");
+    supreme.appendChild(supremeInfo);
+
+    const supremeName = document.createElement("h3");
+    supremeName.textContent = "SUPREME -";
+    supremeInfo.appendChild(supremeName);
+
+    const supremePrices = document.createElement("p");
+    supremePrices.classList.add("pizza-price");
+    supremePrices.textContent = "Per slice: $4, Whole pie: $22.00";
+    supremeInfo.appendChild(supremePrices);
+
+    const supremeIngredients = document.createElement("p");
+    supremeIngredients.textContent = "(Pizza sauce, mozzarella, bacon, onion, beef mince, capsicum, pepperoni, mushroom, olives)";
+    supreme.appendChild(supremeIngredients);   
+
+    // Hawaiian 
+    const hawaiian = document.createElement("div");
+    hawaiian.classList.add("pizza");
+    pizzaDetails.appendChild(hawaiian);
+
+    const hawaiianInfo = document.createElement("div");
+    hawaiianInfo.classList.add("pizza-info");
+    hawaiian.appendChild(hawaiianInfo);
+
+    const hawaiianName = document.createElement("h3");
+    hawaiianName.textContent = "HAWAIIAN -";
+    hawaiianInfo.appendChild(hawaiianName);
+
+    const hawaiianPrices = document.createElement("p");
+    hawaiianPrices.classList.add("pizza-price");
+    hawaiianPrices.textContent = "Per slice: $4.5, Whole pie: $22.50";
+    hawaiianInfo.appendChild(hawaiianPrices);
+
+    const hawaiianIngredients = document.createElement("p");
+    hawaiianIngredients.textContent = "(Pizza sauce, mozzarella, ham, pineapple)";
+    hawaiian.appendChild(hawaiianIngredients); 
+    
+    //Garlic butter prawns and chilli
+    const garlicPrawnsChilli = document.createElement("div");
+    garlicPrawnsChilli.classList.add("pizza");
+    pizzaDetails.appendChild(garlicPrawnsChilli);
+
+    const garlicPrawnsChilliInfo = document.createElement("div");
+    garlicPrawnsChilliInfo.classList.add("pizza-info");
+    garlicPrawnsChilli.appendChild(garlicPrawnsChilliInfo);
+
+    const garlicPrawnsChilliName = document.createElement("h3");
+    garlicPrawnsChilliName.textContent = "GARLIC BUTTER PRAWNS HOT -";
+    garlicPrawnsChilliInfo.appendChild(garlicPrawnsChilliName);
+
+    const garlicPrawnsChilliPrices = document.createElement("p");
+    garlicPrawnsChilliPrices.classList.add("pizza-price");
+    garlicPrawnsChilliPrices.textContent = "Per slice: $4.5, Whole pie: $22.50";
+    garlicPrawnsChilliInfo.appendChild(garlicPrawnsChilliPrices);
+
+    const garlicPrawnsChilliIngredients = document.createElement("p");
+    garlicPrawnsChilliIngredients.textContent = "(Pizza sauce, mozzarella, garlic butter prawns, capsicum, onion, chilli, rocket)";
+    garlicPrawnsChilli.appendChild(garlicPrawnsChilliIngredients);    
+
+    //Margherita
+    const margherita = document.createElement("div");
+    margherita.classList.add("pizza");
+    pizzaDetails.appendChild(margherita);
+
+    const margheritaInfo = document.createElement("div");
+    margheritaInfo.classList.add("pizza-info");
+    margherita.appendChild(margheritaInfo);
+
+    const margheritaName = document.createElement("h3");
+    margheritaName.textContent = "MARGHERITA -";
+    margheritaInfo.appendChild(margheritaName);
+
+    const margheritaPrices = document.createElement("p");
+    margheritaPrices.classList.add("pizza-price");
+    margheritaPrices.textContent = "Per slice: $2, Whole pie: $18";
+    margheritaInfo.appendChild(margheritaPrices);
+
+    const margheritaIngredients = document.createElement("p");
+    margheritaIngredients.textContent = "(Pizza sauce, buffalo mozzarella, basil, olive oil, salt)";
+    margherita.appendChild(margheritaIngredients);  
+    
+    //Quattro Formaggio
+    const fourCheese = document.createElement("div");
+    fourCheese.classList.add("pizza");
+    pizzaDetails.appendChild(fourCheese);
+
+    const fourCheeseInfo = document.createElement("div");
+    fourCheeseInfo.classList.add("pizza-info");
+    fourCheese.appendChild(fourCheeseInfo);
+
+    const fourCheeseName = document.createElement("h3");
+    fourCheeseName.textContent = "QUATTRO FORMAGGIO -";
+    fourCheeseInfo.appendChild(fourCheeseName);
+
+    const fourCheesePrices = document.createElement("p");
+    fourCheesePrices.classList.add("pizza-price");
+    fourCheesePrices.textContent = "Per slice: $2.5, Whole pie: $19.50";
+    fourCheeseInfo.appendChild(fourCheesePrices);
+
+    const fourCheeseIngredients = document.createElement("p");
+    fourCheeseIngredients.textContent = "(Mozzarella, parmesan, provolone, blue cheese)";
+    fourCheese.appendChild(fourCheeseIngredients);  
+
+    //prosciutto 
+    const prosciutto = document.createElement("div");
+    prosciutto.classList.add("pizza");
+    pizzaDetails.appendChild(prosciutto);
+
+    const prosciuttoInfo = document.createElement("div");
+    prosciuttoInfo.classList.add("pizza-info");
+    prosciutto.appendChild(prosciuttoInfo);
+
+    const prosciuttoName = document.createElement("h3");
+    prosciuttoName.textContent = "PROSCIUTTO AND ROCKET -";
+    prosciuttoInfo.appendChild(prosciuttoName);
+
+    const prosciuttoPrices = document.createElement("p");
+    prosciuttoPrices.classList.add("pizza-price");
+    prosciuttoPrices.textContent = "Per slice: $3.5, Whole pie: $20.50";
+    prosciuttoInfo.appendChild(prosciuttoPrices);
+
+    const prosciuttoIngredients = document.createElement("p");
+    prosciuttoIngredients.textContent = "(Pizza sauce, mozzarella, prosciutto, rocket/arugula, extra virgin olive oil)";
+    prosciutto.appendChild(prosciuttoIngredients);  
+
     pizzaRow.appendChild(pizzaDetails);
+    // Pizza end
 
     const calzoneRow = document.createElement("div");
     calzoneRow.classList.add("menu-row-reverse");
